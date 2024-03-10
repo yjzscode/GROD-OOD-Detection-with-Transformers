7889c5832206f35869768eefe4532391a6891716
ZhouYijin <102683638+ZhouYijin@users.noreply.github.com>
2024-03-11 01:54:19 +0800

    Add files via upload

--- codes/dataset/generate_data.py
+++ codes/dataset/generate_data.py
@@ -7,7 +7,8 @@ from omegaconf import OmegaConf
 
 config_path = "D:\Academic\毕设\codes\config\config0.yaml"
 config = OmegaConf.load(config_path)
-
+from torch.utils.data import Dataset
+# (N,d0)
 
 class generate_train_data:
     def __init__(self, d0, n, seed, K):
@@ -18,7 +19,7 @@ class generate_train_data:
         self.seed = seed
         self.num_class = K
 
-    def generate(self):
+    def generate(self, mean_list, std_list):
 
         # 设置随机种子
         torch.manual_seed(self.seed)
@@ -28,27 +29,14 @@ class generate_train_data:
         # 数据维度 self.d0
         # 总数据量 self.n
         cluster_data = []
-
-        # 生成第 K+1 簇数据
-        far_mean = torch.randn(self.d0) * 20  # 选择一个远离前 n 簇数据的均值
-        far_std = torch.rand(self.d0) + 1.0  # 随机生成标准差
-        far_cluster_size = torch.randint(low=50, high=100, size=(1,))  # 第 n+1 簇数据点数量
-        far_cluster = torch.randn(far_cluster_size, self.d0) * far_std + far_mean
-        # 清理数据：删除超出三个标准差范围之外的数据点
-        mask = (far_cluster > (far_mean - 3 * far_std)) & (far_cluster < (far_mean + 3 * far_std))
-        mask = mask.all(dim=1)
-        far_cluster = far_cluster[mask]           
-        
+        label = []
+ 
         # 生成 ID 数据
-        n_ID = self.n - far_cluster_size
-        mean_list = []
-        std_list = []
+        n_ID = self.n 
         for i in range(self.num_class):
-            mean = torch.randn(self.d0) * 5  # 随机生成每个簇的均值
-            std = torch.rand(self.d0) + 1.0  # 随机生成每个簇的标准差
-            mean_list.append(mean)
-            std_list.append(std)
-
+            mean = mean_list[i]  # 随机生成每个簇的均值
+            std = std_list[i] # 随机生成每个簇的标准差
+    
             cluster_size = int(n_ID // self.num_class)  # 每个簇的数据点数量
             cluster = torch.randn(cluster_size, self.d0) * std + mean
             
@@ -57,11 +45,68 @@ class generate_train_data:
             mask = mask.all(dim=1)
             cleaned_cluster = cluster[mask]           
             cluster_data.append(cleaned_cluster)
+            label.append([i+1]*cleaned_cluster[0])
         
+
+        # 调整每个簇的数据数量，使得所有簇的数据总量为 N
+        total_points = sum(len(cluster) for cluster in cluster_data)
+        if total_points < self.n:
+            # 增加数据点
+            remaining_points = self.n - total_points
+            for i in range(remaining_points):
+                idx = i % (self.num_class)  # 循环添加数据点以平衡各个簇的数据数量
+                new_point = torch.randn(1, self.d0) * std_list[idx] + mean_list[idx]
+                cluster_data[idx] = torch.cat([cluster_data[idx], new_point])
+                label.append([idx+1])
+        return cluster_data, label
+    
+class generate_test_data:
+    def __init__(self, d0, n, seed, K):
+        super(generate_test_data, self).__init__()
+
+        self.d0 = d0
+        self.n = n
+        self.seed = seed
+        self.num_class = K
+
+    def generate(self, mean_list, std_list):
+
+        # 设置随机种子
+        torch.manual_seed(self.seed)
+
+        # 生成 K 个簇的ID数据，一簇OOD数据
+        # 簇的数量 self.num_class
+        # 数据维度 self.d0
+        # 总数据量 self.n
+        cluster_data = []
+        label = []
+        # 生成第 K+1 簇数据
+        far_mean = mean_list[self.K]  
+        far_std = std_list[self.K]    # 随机生成标准差
+        far_cluster_size = int(self.n // (self.num_class + 1))
+        far_cluster = torch.randn(far_cluster_size, self.d0) * far_std + far_mean
+        # 清理数据：删除超出三个标准差范围之外的数据点
+        mask = (far_cluster > (far_mean - 3 * far_std)) & (far_cluster < (far_mean + 3 * far_std))
+        mask = mask.all(dim=1)
+        far_cluster = far_cluster[mask]           
+        
+        # 生成 ID 数据
+        n_ID = self.n - far_cluster_size
+        for i in range(self.num_class):
+            mean = mean_list[i]  # 随机生成每个簇的均值
+            std = std_list[i] # 随机生成每个簇的标准差
+    
+            cluster_size = int(n_ID // self.num_class)  # 每个簇的数据点数量
+            cluster = torch.randn(cluster_size, self.d0) * std + mean
+            
+            # 清理数据：删除超出三个标准差范围之外的数据点
+            mask = (cluster > (mean - 3 * std)) & (cluster < (mean + 3 * std))
+            mask = mask.all(dim=1)
+            cleaned_cluster = cluster[mask]           
+            cluster_data.append(cleaned_cluster)
+            label.append([i+1]*cleaned_cluster[0])
         # 加上OOD数据
-        cluster_data.append(far_cluster)
-        mean_list.append(far_mean)
-        std_list.append(far_std)
+        cluster_data.append([self.num_class+1] * far_cluster[0])
 
         # 调整每个簇的数据数量，使得所有簇的数据总量为 N
         total_points = sum(len(cluster) for cluster in cluster_data)
@@ -72,14 +117,50 @@ class generate_train_data:
                 idx = i % (self.num_class + 1)  # 循环添加数据点以平衡各个簇的数据数量
                 new_point = torch.randn(1, self.d0) * std_list[idx] + mean_list[idx]
                 cluster_data[idx] = torch.cat([cluster_data[idx], new_point])
-        return cluster_data
+                label.append([idx+1])
+        return cluster_data, label
 
+#############################################################组合
+class myDataset(Dataset):
+    def __init__(self, data, label):
+        self.data = torch.tensor(data).unsqueeze(-1)  # n = 1 (N,d0,1)
+        #5个数据的标签
+        self.label = torch.tensor(label)
+    
+    #根据索引获取data和label
+    def __getitem__(self,index):
+        return self.data[index], self.label[index] #以元组的形式返回
 
-train_data_func = generate_train_data(config.d0, config.n * 0.8, config.seed_data, config.K)
-test_data_func = generate_train_data(config.d0, config.n * 0.8, config.seed_data, config.K)
+    #获取数据集的大小
+    def __len__(self):
+        return len(self.data)
+###########################################################
+    
+train_data_func = generate_train_data(config.d0, config.N * 0.8, config.seed_data, config.K)
+test_data_func = generate_test_data(config.d0, config.N * 0.2, config.seed_data, config.K)
 
-train_data = train_data_func()
-test_data = test_data_func()
+mean_list = []
+std_list = []
+for i in range(config.K):
+    mean = torch.randn(config.d0) * 5  # 随机生成每个簇的均值
+    std = torch.rand(config.d0) + 1.0  # 随机生成每个簇的标准差
+    mean_list.append(mean)
+    std_list.append(std)
+mean = torch.randn(config.d0) * 200  # 随机生成每个簇的均值
+std = torch.rand(config.d0) + 1.0  # 随机生成每个簇的标准差
+mean_list.append(mean)
+std_list.append(std)
+
+train_data = train_data_func(mean_list, std_list)[0]
+train_label = train_data_func(mean_list, std_list)[1]
+
+test_data = test_data_func(mean_list, std_list)[0]
+test_label = test_data_func(mean_list, std_list)[1]
+
+data = myDataset(train_data, train_label)
+print(f'data size is : {len(data)}')
+
+print(data[1]) #获取索引为1的data和label
 
 # 可视化数据 2维
 plt.figure(figsize=(8, 6))
@@ -89,7 +170,3 @@ plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Cleaned and Adjusted Data')
 plt.show()
-
-# 大于2维可以PCA
-# Note: 随机种子固定，test会是train的一部分吗？
-# 标签还没加!!!
--- codes/model/Transformer.py
+++ codes/model/Transformer.py
@@ -5,12 +5,13 @@ from einops import repeat
 
 
 class SelfAttention(nn.Module): #单个head
-    def __init__(self, m_V, m_h, d, b):
+    def __init__(self, m_V, m_h, d, n, batch_size):
         super(SelfAttention, self).__init__()
         self.m_V = m_V
         self.m_h = m_h
         self.d = d
-        self.b = b
+        self.n = n
+        self.batch_size = batch_size
         self.W_O = nn.Parameter(
             torch.tensor(
                 torch.randn(self.d, self.m_V), dtype=torch.float32, requires_grad=requires_grad
@@ -32,8 +33,9 @@ class SelfAttention(nn.Module): #单个head
             )
         )
 
-    def forward(self, x): # shape of x: (d, b)
+    def forward(self, x): # shape of x: (b, d, n)
         # perform linear operation and split into h heads
+
         k = torch.matmul(self.W_K, x)
         q = torch.matmul(self.W_Q, x)
         v = torch.matmul(self.W_V, x)
@@ -78,91 +80,102 @@ class FFN(nn.Module):
         self.relu = nn.ReLU()
     def forward(self, x):
         act = self.relu(torch.matmul(self.W_1, x)+ torch.matmul(self.b_1.T, torch.ones(self.r)))
-        output = x + torch.matmul(self.W_2, act) + torch.matmul(self.b_2.T, torch.ones(self.d)))
+        output = x + torch.matmul(self.W_2, act) + torch.matmul(self.b_2.T, torch.ones(self.d))
         return output
 
-class FCNN_layer(nn.Module): #x: (d0, b)->(d, b)
+class FCNN_layer(nn.Module): #x: (b, d0, n)->(b, d, n)
     def __init__(self, d0, d):
         super(FCNN_layer, self).__init__()
         self.d = d
         self.linear = nn.Linear(d0, d)
         self.relu = nn.ReLU()
     def forward(self, x):
-        x = self.linear(x.T).T
+        x = torch.transpose(self.linear(torch.transpose(x, -1, -2)), -1, -2)
         x = self.relu(x)
         return x
 
 
-class classifier(nn.Module): #x: (d0, b)->(d, b)
-    def __init__(self, d, K, mode_c, mode_E, lambda0, T, epsilon=1e-7):
+class classifier(nn.Module): 
+    def __init__(self, d, K, mode_c, mode_E, lambda0, T, batch_size, n, epsilon=1e-7):
         super(classifier, self).__init__()
         self.epsilon = epsilon
         self.d = d
         self.K = K
         self.T = T
+        self.n = n
+        self.batch_size = batch_size
         self.mode_c = mode_c
         self.mode_E = mode_E
-        self.linear = nn.Linear(d, K+1)
-        self.lambda0 = nn.Parameter(
-            torch.tensor(
-                [lambda0], dtype=torch.float32, requires_grad=requires_grad
-            )
-        )
-    def scoring_function(self, x, mode_E, T):#(d, b)
+        self.linear = nn.Linear(d, 1)
+        self.linear1 = nn.Linear(self.n,K+1)
+        self.lambda0 = lambda0
+        # self.lambda0 = nn.Parameter(
+        #     torch.tensor(
+        #         [lambda0], dtype=torch.float32, requires_grad=requires_grad
+        #     )
+        # )
+    def scoring_function(self, x, mode_E, T):#(b, K+1)
         if mode_E == 'e': 
-            sum = torch.sum(torch.exp(x), dim = 0)
-            sum = repeat(sum, "b -> d b", b = x.size[1], d = x.size()[0])
+            sum = torch.sum(torch.exp(x), dim = 1)
+            sum = repeat(sum, "b -> b d", b = self.batch_size, d = x.size()[1])
             x = torch.exp(x) / sum
-            output = torch.max(x, dim = 0)
+            output = torch.max(x, dim = 1) #b
         elif mode_E == 'e_T':
-            sum = torch.sum(torch.exp(x / T), dim = 0)
-            sum = repeat(sum, "b -> d b", b = x.size[1], d = x.size()[0])
+            sum = torch.sum(torch.exp(x / T), dim = 1)
+            sum = repeat(sum, "b -> b d", b = self.batch_size, d = x.size()[1])
             x = torch.exp(x / T) / sum
-            output = torch.max(x, dim = 0)
+            output = torch.max(x, dim = 1) #b
         elif mode_E == 'log_T':
-            output = T * torch.log(torch.sum(torch.exp(x / T), dim = 0))
+            output = T * torch.log(torch.sum(torch.exp(x / T), dim = 1))
         else:
             print('no such kind of scoring function')
         return output #b
     
-    def forward(self, x):
-        x = self.linear(x.T).T 
+    def forward(self, x): #x: (b, d0, n)->b
+        x = self.linear(torch.transpose(x, -1, -2)) #(b,n,1)
+        x = torch.squeeze(x,2) #(b,n)
+        x = self.linear1(x) #(b,K+1)
         if self.mode_c == 'max':
-            output = torch.argmax(x, dim = 0) + 1 #1, ..., K+1
-        elif self.mode_c == 'score': #(d, b)
-            score = self.scoring_function(x, self.mode_E, self.T) #b
-            score = repeat(score, "b -> d b", b = x.size[1], d = x.size()[0])
-            OOD = torch.ones(x.size()) * (2 / self.epsilon)
+            output = torch.argmax(x, dim = 1) + 1 #1, ..., K+1 b   
+            return output, x
+        elif self.mode_c == 'score': #(d, n)
+            score0 = self.scoring_function(x, self.mode_E, self.T) #b
+            score = repeat(score0, "b-> b d", b = self.batch_size, d = x.size()[1]) 
+            OOD = torch.zeros(x.size())
+            OOD[:,-1] = torch.ones(self.batch_size)
+            x[:,-1] = torch.zeros(self.batch_size)
             biclas_matrix = torch.where(
                 torch.lt(score, self.lambda0),         
                 OOD, 
                 x
                 )
-            max_value = torch.max(biclas_matrix[:-1], dim = 0)
-            output = torch.argmax(biclas_matrix[:-1], dim = 0) + 1
+            max_value = torch.max(biclas_matrix[:-1], dim = 1) # b
+            hat_c = torch.argmax(biclas_matrix[:-1], dim = 1) + 1
             output = torch.where(
-                torch.gt(max_value, 1/self.epsilon),
-                (self.K + 1) * torch.ones(x.size()[1]),
-                output
-                )
+                torch.lt(max_value, self.epsilon),
+                (self.K + 1) * torch.ones(self.batch_size),
+                hat_c
+                ) #b 
+            return output, biclas_matrix   
+
         else:
             print('no such kind of classifier')
-        return output #b
+        
 
 
 ###########################################################
-#SelfAttention(self, m_V, m_h, d, b):
+#SelfAttention(self, m_V, m_h, d, n, batch_size):
 #FFN(self, d, r):     
 #FCNN_layer(self, d0, d):
-#classifier(self, d, K, mode_c, mode_E, lambda0, T):
+#classifier(self, d, K, mode_c, mode_E, lambda0, T, batch_size):
    
 class TransformerBlock(nn.Module):  
-    def __init__(self, m_V, m_h, d, b, r, h):
+    def __init__(self, m_V, m_h, d, n, r, h, batch_size):
         super(TransformerBlock, self).__init__()
         self.h = h
         self.layers = []
         for _ in range(h):
-            self.layers.append(SelfAttention(m_V, m_h, d, b))
+            self.layers.append(SelfAttention(m_V, m_h, d, n, batch_size))
         self.FFN = FFN(d, r)
         
     def forward(self, x):
@@ -172,13 +185,13 @@ class TransformerBlock(nn.Module):
         output = self.FFN(attn_output)
         return output
 
-
+# (b, d0, n)->b
 class Transformer(nn.Module):  
     def __init__(self, 
                  m_V, 
                  m_h, 
                  d, 
-                 b, 
+                 n, 
                  r, 
                  h, 
                  d0, 
@@ -187,17 +200,18 @@ class Transformer(nn.Module):
                  mode_E, 
                  lambda0, 
                  T, 
-                 l
+                 l,
+                 batch_size
                  ):
         super(Transformer, self).__init__()
         layers = []
         for _ in range(l):
-            layers.append(TransformerBlock(m_V, m_h, d, b, r, h))
+            layers.append(TransformerBlock(m_V, m_h, d, n, r, h, batch_size))
         self.layers = nn.Sequential(*layers)
         self.fcnn = FCNN_layer(d0, d)
-        self.classifier = classifier(self, d, K, mode_c, mode_E, lambda0, T)
+        self.classifier = classifier(self, d, K, mode_c, mode_E, lambda0, T, batch_size,n)
     def forward(self, x):
         x = self.fcnn(x)
         x = self.layers(x)
-        output = self.classifier(x)    
-        return(output)
+        output, label_matrix = self.classifier(x)    
+        return output, label_matrix 
