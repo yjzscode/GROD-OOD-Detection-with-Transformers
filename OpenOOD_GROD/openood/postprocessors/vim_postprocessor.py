70a7fdd2e787ae6ea2519e11b90f33c49c6da08a
YIJIN ZHOU <102683638+yjzscode@users.noreply.github.com>
2025-01-19 15:47:54 +0800

    Update vim_postprocessor.py
    v2

--- OpenOOD_GROD/openood/postprocessors/vim_postprocessor.py
+++ OpenOOD_GROD/openood/postprocessors/vim_postprocessor.py
@@ -1,120 +1,56 @@
 from typing import Any
+from tqdm import tqdm
 
-import numpy as np
 import torch
 import torch.nn as nn
-from numpy.linalg import norm, pinv
-from scipy.special import logsumexp
-from sklearn.covariance import EmpiricalCovariance
-from tqdm import tqdm
-import torch
-from .base_postprocessor import BasePostprocessor
+from torch.utils.data import DataLoader
 
-### modified for GROD ###
+import openood.utils.comm as comm
 
-class VIMPostprocessor(BasePostprocessor):
+### Modified by GROD ###
+class VIMPostprocessor: #base version
     def __init__(self, config):
-        super().__init__(config)
-        self.args = self.config.postprocessor.postprocessor_args
-        self.args_dict = self.config.postprocessor.postprocessor_sweep
-        self.dim = self.args.dim
-        self.setup_flag = False
+        self.config = config
 
     def setup(self, net: nn.Module, id_loader_dict, ood_loader_dict):
-        if not self.setup_flag:
-            net.eval()
-
-            with torch.no_grad():
-                # self.w, self.b = net.backbone.get_fc()
-                # print(self.b.size)
-                self.w, self.b = net.head.weight[:-1,:].cpu().numpy(), net.head.bias[:-1].cpu().numpy()
-                # print(self.w.size())
-                print('Extracting id training feature')
-                feature_id_train = []
-                logit_id_train = []
-                for batch in tqdm(id_loader_dict['train'],
-                                  desc='Setup: ',
-                                  position=0,
-                                  leave=True):
-                    data = batch['data'].cuda()
-                    data = data.float()
-                    _, feature = net.backbone(data, return_feature=True)
-                    logit = net.head(feature)
-                    score = torch.softmax(logit, dim=1)
-                    score0 = torch.softmax(logit[:,:-1], dim=1)
-                    conf, pred = torch.max(score, dim=1)
-                    conf0, pred0 = torch.max(score0, dim=1)
-                    for i in range(pred.size(0)):
-                        if pred[i] == logit.size(1) - 1:
-                            conf[i] = 0.1
-                            pred[i] = 1
-                            score0[i, :] = 0.1 * torch.ones(score0.size(1)).cuda()
-                        else:
-                            conf[i] = conf0[i]     
-                        
-                    feature_id_train.append(feature.cpu().numpy())
-                    logit_id_train.append(score0.cpu().numpy())
-                feature_id_train = np.concatenate(feature_id_train, axis=0)
-                logit_id_train = np.concatenate(logit_id_train, axis=0)
-
-                # logit_id_train = feature_id_train @ self.w.T + self.b
-
-            self.u = -np.matmul(pinv(self.w), self.b)
-            
-            feature_id_train_tensor = torch.tensor(feature_id_train, dtype=torch.float32).cuda()
-            u_tensor = torch.tensor(self.u, dtype=torch.float32).cuda()
-            
-            centered_data_tensor = feature_id_train_tensor - u_tensor
-            empirical_covariance = torch.matmul(centered_data_tensor.t(), centered_data_tensor) / centered_data_tensor.size(0)
-            
-            eig_vals, eigen_vectors = torch.linalg.eigh(empirical_covariance)#, eigenvectors=True)
-            eig_vals = eig_vals.cpu().detach().numpy()
-            eigen_vectors = eigen_vectors.cpu().detach().numpy()
-
-            
-            self.NS = np.ascontiguousarray(
-                (eigen_vectors.T[np.argsort(eig_vals * -1)[self.dim:]]).T)
-
-            vlogit_id_train = norm(np.matmul(feature_id_train - self.u,
-                                             self.NS),
-                                   axis=-1)
-            self.alpha = logit_id_train.max(
-                axis=-1).mean() / vlogit_id_train.mean()
-            print(f'{self.alpha=:.4f}')
-
-            self.setup_flag = True
-        else:
-            pass
+        pass
 
     @torch.no_grad()
-    def postprocess(self, net: nn.Module, data: Any):
-        _, feature_ood = net.backbone.forward(data, return_feature=True)
-        
-        logit = net.head(feature_ood)
-        score = torch.softmax(logit, dim=1)
-        score0 = torch.softmax(logit[:,:-1], dim=1)
+    def postprocess(self, net: nn.Module, data: Any, target: Any):
+        output = net(data, target)[0] #(b, 768)
+        output = net.head(output) #(b, 11)
+
+        score = torch.softmax(output, dim=1)
+        score0 = torch.softmax(output[:,:-1], dim=1)
         conf, pred = torch.max(score, dim=1)
         conf0, pred0 = torch.max(score0, dim=1)
         for i in range(pred.size(0)):
-          if pred[i] == logit.size(1) - 1:
-            conf[i] = 0.1
-            pred[i] = 1
-            score0[i, :] = 0.1 * torch.ones(score0.size(1)).cuda()
-          else:
-            conf[i] = conf0[i]
-        logit_ood = score0.cpu()    
-        
-        feature_ood = feature_ood.cpu()
-        # logit_ood = feature_ood @ self.w.T + self.b
-        _, pred = torch.max(logit_ood, dim=1)
-        energy_ood = logsumexp(logit_ood.numpy(), axis=-1)
-        vlogit_ood = norm(np.matmul(feature_ood.numpy() - self.u, self.NS),
-                          axis=-1) * self.alpha
-        score_ood = -vlogit_ood + energy_ood
-        return pred, torch.from_numpy(score_ood)
+            if pred[i] == output.size(1) - 1:
+                conf[i] = 0.01
+                pred[i] = 1
+            else:
+                conf[i] = conf0[i]                
 
-    def set_hyperparam(self, hyperparam: list):
-        self.dim = hyperparam[0]
+        return pred, conf
 
-    def get_hyperparam(self):
-        return self.dim
+    def inference(self,
+                  net: nn.Module,
+                  data_loader: DataLoader,
+                  progress: bool = True):
+        pred_list, conf_list, label_list = [], [], []
+        for batch in tqdm(data_loader,
+                          disable=not progress or not comm.is_main_process()):
+            data = batch['data'].cuda()
+            label = batch['label'].cuda()
+            pred, conf = self.postprocess(net, data, label)
+
+            pred_list.append(pred.cpu())
+            conf_list.append(conf.cpu())
+            label_list.append(label.cpu())
+
+        # convert values into numpy array
+        pred_list = torch.cat(pred_list).numpy().astype(int)
+        conf_list = torch.cat(conf_list).numpy()
+        label_list = torch.cat(label_list).numpy().astype(int)
+
+        return pred_list, conf_list, label_list
